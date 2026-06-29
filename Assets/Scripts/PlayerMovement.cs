a0218c46995a1f28dc58851716d8efe45f2a3d32
gitDemo <sjm0091@auburn.edu>
2026-06-29 00:34:56 -0500

    Implemented Jumping
    Implemented Jumping
    - Crouch not implemented
    - waiting until I have an animated player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Callbacks;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.TextCore.Text;
 
@@ -11,12 +12,17 @@ public class PlayerMovement : MonoBehaviour
 {
     private Vector2 moveInput;
     public Vector2 rotateInput;
+    public Vector2 jumpCrouchInput;
+    public Transform raycastStart;
     private CharacterController characterController;
     private Rigidbody rb;
     public float moveSpeed = 5f;
+    public float jumpForce = 1f;
     public float rotationSpeed = 0.5f;
 
     float gravity = -9.8f;
+
+    bool isGrounded;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -36,8 +42,21 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 movement = (((transform.forward * moveInput.y) + (transform.right * moveInput.x)).normalized * moveSpeed) + new Vector3(0f, rb.linearVelocity.y, 0f);
         
-        Debug.Log("toMove: " + movement);
+        // Debug.Log("toMove: " + movement);
         rb.linearVelocity = movement;
+
+        if (jumpCrouchInput.y != 0)
+        {
+            // Debug.Log("jump activated");
+            if (jumpCrouchInput.y > 0)
+            {
+                Jump();
+            }
+            if (jumpCrouchInput.y < 0)
+            {
+                Crouch();
+            }
+        }
 
         
     }
@@ -50,5 +69,36 @@ public class PlayerMovement : MonoBehaviour
     public void OnRotate(InputValue value)
     {
         rotateInput = value.Get<Vector2>();
+    }
+
+    public void OnJumpCrouch(InputValue value)
+    {
+        jumpCrouchInput = value.Get<Vector2>();
+    }
+
+    public void CheckIfGrounded()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, 1.5f))
+        {
+            isGrounded = true;
+        }
+    }
+
+    public void Jump()
+    {
+        CheckIfGrounded();
+        Debug.Log("isGrounded: "+isGrounded);
+        if (isGrounded)
+        {
+            Vector3 jump = new Vector3(0f, jumpForce, 0f);
+            rb.AddForce(jump, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    public void Crouch()
+    {
+        // To implement once I have an animated player
+        return;
     }
 }
